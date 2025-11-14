225bcfcef5ef24af2e03e0ad0c11b3899c37ab72
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2025-11-15 05:22:07 +0530

    feat: configure Next.js for Walrus content serving

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aggregator.walrus-testnet.walrus.space',
+      },
+    ],
+  },
   async headers() {
     const csp = [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       "style-src 'self' 'unsafe-inline'",
-      "img-src 'self' data: blob:",
-      "connect-src 'self' https://fullnode.devnet.sui.io https://fullnode.testnet.sui.io https://fullnode.mainnet.sui.io https://api.enoki.mystenlabs.com https://oauth2.googleapis.com https://accounts.google.com",
+      "img-src 'self' data: blob: https://aggregator.walrus-testnet.walrus.space",
+      "connect-src 'self' https://fullnode.devnet.sui.io https://fullnode.testnet.sui.io https://fullnode.mainnet.sui.io https://api.enoki.mystenlabs.com https://oauth2.googleapis.com https://accounts.google.com https://aggregator.walrus-testnet.walrus.space https://publisher.walrus-testnet.walrus.space",
       "frame-src https://accounts.google.com",
     ].join('; ');
 
